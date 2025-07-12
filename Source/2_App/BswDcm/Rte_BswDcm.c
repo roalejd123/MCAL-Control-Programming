0b326c1b0e36b91d34f38872889d85f81dca59fd
roalejd123 <roalejd123@naver.com>
2025-07-12 10:31:38 +0900

    first commit

---
+++ Source/2_App/BswDcm/Rte_BswDcm.c
@@ -0,0 +1,24 @@
+#include "Rte_BswDcm.h"
+#include "Definition.h"
+#include "UdsNrcList.h"
+#include "Utility.h"
+
+void REcbBswDcm_initialize(void)
+{
+	(void) 0;
+}
+
+void REtBswDcm_bswDcmMain(void)
+{
+    (void) 0;
+}
+
+void REoiBswDcm_pDcmCmd_processDiagRequest(uint8 isPhysical, P_uint8 diagReq, uint8 length)
+{
+    (void) 0;
+}
+
+void REoiBswDcm_pDcmCmd_getDiagStatus(P_stDiagStatus pstDiagStatus)
+{
+    (void) 0;
+}
